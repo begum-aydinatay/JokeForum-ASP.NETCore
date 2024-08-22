8845730d9ac7814be6bce916e1281a6eb2058411
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-22 12:50:47 +0200

    Created a simple 'joke' database, added authorization for 'create, delete, edit', added 'search' functionality.

--- JokeForum/Data/ApplicationDbContext.cs
+++ JokeForum/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using JokeForum.Models;
 
 namespace JokeForum.Data
 {
@@ -9,5 +10,6 @@ namespace JokeForum.Data
             : base(options)
         {
         }
+        public DbSet<JokeForum.Models.Joke> Joke { get; set; } = default!;
     }
 }
