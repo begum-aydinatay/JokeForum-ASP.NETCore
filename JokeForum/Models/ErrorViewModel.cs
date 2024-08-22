c223fcd20fcb67860af3d1d350f97979bc3230f9
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-22 08:41:17 +0200

    Add project files.

---
+++ JokeForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokeForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
